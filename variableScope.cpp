f2723880eeb3ce2a22bf784ae506d124137456d2
Nerisse Allyztair M. Ramirez <allyztair03@gmail.com>
2024-08-29 12:54:29 +0800

    variableScope done

--- variableScope.cpp
+++ variableScope.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+int myNum = 3;
+void printNum();
+
 int main()
 {
+    // Local variables = delcared inside a function or block {}
+    // Global variables = delcared outside of all functions
+
+    int myNum = 2;
+    printNum();
+    std::cout << ::myNum << '\n'; // 2 colons before the variable = scope for global variables
+
     return 0;
+}
+
+void printNum()
+{
+    int myNum = 1;
+    std::cout << ::myNum << '\n'; // 2 colons before the variable = scope for global variables
 }
